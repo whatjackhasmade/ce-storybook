c0434864672f0551c536c372799349e70fdbe940
Jack Pritchard <jack@noface.co.uk>
2019-12-20 12:33:46 +0000

    Add page and post templates

---
+++ templates/page.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const PageTemplate = props => (
+  <>
+    <h1>{props.pageContext.title}</h1>
+  </>
+)
+
+export default PageTemplate
---
+++ templates/post.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const PostTemplate = props => (
+  <>
+    <h1>{props.pageContext.title}</h1>
+  </>
+)
+
+export default PostTemplate
