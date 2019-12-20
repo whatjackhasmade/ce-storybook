9290578eafc9552253e3e55b9e53a1b3c7696089
Jack Pritchard <jack@noface.co.uk>
2019-12-20 13:25:05 +0000

    Create button component

--- src/components/atoms/button/button.jsx
+++ src/components/atoms/button/button.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Button = ({ children }) => <button className="button">{children}</button>;
+const Button = ({ children, href, onClick }) => {
+	if (!href)
+		return (
+			<button className="button" onClick={onClick}>
+				{children}
+			</button>
+		);
+	return (
+		<a className="button" href={href}>
+			{children}
+		</a>
+	);
+};
+
+// Expected prop values
+Button.propTypes = {
+	children: PropTypes.node.isRequired,
+	href: PropTypes.string,
+	onClick: PropTypes.func
+};
+
+// Default prop values
+Button.defaultProps = {
+	children: "Button text"
+};
 
 export default Button;
--- src/components/atoms/button/button.stories.js
+++ src/components/atoms/button/button.stories.js
@@ -1,7 +1,16 @@
 import React from "react";
 import Button from "./button";
 
+const alertText = e => {
+	e.preventDefault();
+	alert("You clicked the button");
+};
+
 export const basicButton = () => <Button>Basic button</Button>;
+export const functionButton = () => (
+	<Button onClick={alertText}>Click me</Button>
+);
+export const linkedButton = () => <Button href="/route">Link to route</Button>;
 
 export default {
 	component: Button,
