00c23958ab9ae7adc5cb4f9435b9092ea289d55f
Jack Pritchard <jack@noface.co.uk>
2019-12-20 13:08:58 +0000

    Add Storybook files

---
+++ src/components/atoms/button/button.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "./button";
+
+export const basicButton = () => <Button>Basic button</Button>;
+
+export default {
+	component: Button,
+	title: "Button"
+};
