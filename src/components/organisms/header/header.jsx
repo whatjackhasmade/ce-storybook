e574dedf4ee6456c1c22785d8abed7a58f9f1357
Jack Pritchard <jack@noface.co.uk>
2019-12-24 14:27:26 +0000

    Add header variant

--- src/components/organisms/header/header.jsx
+++ src/components/organisms/header/header.jsx
@@ -1,56 +1,60 @@
-import React, { useState } from "react"
-import PropTypes from "prop-types"
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-import StyledHeader from "./header.styles"
+import StyledHeader from "./header.styles";
 
-import BrandLogo from "../../../assets/images/brand/logo.png"
+import BrandLogo from "../../../assets/images/brand/logo.png";
 
-import Link from "../../atoms/link/link"
+import Link from "../../atoms/link/link";
 
-import Navigation from "../../molecules/navigation/navigation"
+import Navigation from "../../molecules/navigation/navigation";
 
-const { arrayOf, shape, string } = PropTypes
+const { arrayOf, shape, string } = PropTypes;
 
-const Header = ({ navigation }) => {
-  const [isOpen, setOpen] = useState(false)
+const Header = props => {
+	const { navigation } = props;
+	const [isOpen, setOpen] = useState(false);
 
-  const toggleNavigation = e => {
-    e.preventDefault()
-    setOpen(!isOpen)
-  }
+	const toggleNavigation = e => {
+		e.preventDefault();
+		setOpen(!isOpen);
+	};
 
-  return (
-    <StyledHeader className={isOpen ? `header--open` : `header--closed`}>
-      <button onClick={toggleNavigation}>
-        {isOpen ? `Hide` : `Show`} Navigation
-      </button>
-      {navigation &&
-        navigation.length &&
-        navigation.map(({ items }) => <Navigation items={items} />)}
-      <Link href="/">
-        <img
-          alt="Celtic Elements Logo"
-          className="header__logo"
-          src={BrandLogo}
-        />
-      </Link>
-    </StyledHeader>
-  )
-}
+	return (
+		<StyledHeader
+			{...props}
+			className={isOpen ? `header--open` : `header--closed`}
+		>
+			<button onClick={toggleNavigation}>
+				{isOpen ? `Hide` : `Show`} Navigation
+			</button>
+			{navigation &&
+				navigation.length &&
+				navigation.map(({ items }) => <Navigation items={items} />)}
+			<Link href="/">
+				<img
+					alt="Celtic Elements Logo"
+					className="header__logo"
+					src={BrandLogo}
+				/>
+			</Link>
+		</StyledHeader>
+	);
+};
 
 // Expected prop values
 Header.propTypes = {
-  navigation: arrayOf(
-    shape({
-      items: arrayOf(
-        shape({
-          label: string,
-          target: string,
-          url: string,
-        })
-      ),
-    })
-  ),
-}
+	navigation: arrayOf(
+		shape({
+			items: arrayOf(
+				shape({
+					label: string,
+					target: string,
+					url: string
+				})
+			)
+		})
+	)
+};
 
-export default Header
+export default Header;
--- src/components/organisms/header/header.stories.js
+++ src/components/organisms/header/header.stories.js
@@ -1,14 +1,47 @@
 import React from "react";
 import { array } from "@storybook/addon-knobs";
+import { WithFigma } from "storybook-addon-figma";
 import Header from "./header";
 
 import knobData from "./header.knobs.json";
 const { headerNav } = knobData;
 
 export const standardHeader = () => (
-	<Header
-		navigation={array(headerNav.label, headerNav.default, headerNav.group)}
-	/>
+	<WithFigma
+		url={
+			"https://www.figma.com/file/uihfnI2u5KSj2LuAVZR7lt/Celtic-Elements?node-id=890%3A2201"
+		}
+	>
+		<Header
+			navigation={array(headerNav.label, headerNav.default, headerNav.group)}
+		/>
+	</WithFigma>
+);
+
+export const fixedDarkHeader = () => (
+	<WithFigma
+		url={
+			"https://www.figma.com/file/uihfnI2u5KSj2LuAVZR7lt/Celtic-Elements?node-id=903%3A96"
+		}
+	>
+		<Header
+			navigation={array(headerNav.label, headerNav.default, headerNav.group)}
+			variant="fixedDark"
+		/>
+	</WithFigma>
+);
+
+export const fixedLightHeader = () => (
+	<WithFigma
+		url={
+			"https://www.figma.com/file/uihfnI2u5KSj2LuAVZR7lt/Celtic-Elements?node-id=903%3A115"
+		}
+	>
+		<Header
+			navigation={array(headerNav.label, headerNav.default, headerNav.group)}
+			variant="fixedLight"
+		/>
+	</WithFigma>
 );
 
 export default {
